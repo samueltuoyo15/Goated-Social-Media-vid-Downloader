037b95239f3e214e542ed423d58d3f1d01ae726c
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-03-05 22:04:06 +0100

    fixed

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -4,6 +4,22 @@ const config: Config = {
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
+  theme: {
+    extend: {
+      animation: {
+        'bounce-sequential': 'bounce-sequential 1.5s infinite',
+      },
+      fontFamily:{
+        sans: ['Inter', 'sans-serif'],
+      },
+      keyframes: {
+        'bounce-sequential': {
+          '0%, 80%, 100%': { transform: 'scale(0)' },
+          '40%': { transform: 'scale(1)' },
+        },
+      },
+    },
+  },
   plugins: [],
 }
 
